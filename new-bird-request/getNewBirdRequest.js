940728e2879ee63333460c7f699b7d082045dcb1
David He <jialhe85@gmail.com>
2018-06-05 10:55:54 +1000

    Set queue url in env in config

--- new-bird-request/getNewBirdRequest.js
+++ new-bird-request/getNewBirdRequest.js
@@ -9,8 +9,7 @@ const receiveMessagePromise = util.promisify(sqs.receiveMessage);
 module.exports.getNewProductRequest = event => {
   receiveMessagePromise
     .call(sqs, {
-      QueueUrl:
-        'https://sqs.us-east-2.amazonaws.com/616874792320/NEW_PROD_REQ-QUEUE',
+      QueueUrl: process.env.QUEUE_URL,
       MaxNumberOfMessages: 10,
     })
     .then(msg => {
